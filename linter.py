4d7bc98d3c436da235a9af954b2e95d63c4fa844
Ben Edwards <ben.edwards@clock.co.uk>
2015-07-30 17:26:32 +0100

    > Update to use version 1.0.0

--- linter.py
+++ linter.py
@@ -10,28 +10,30 @@
 
 """This module exports the JadeLint plugin class."""
 
-from SublimeLinter.lint import Linter, util
+from SublimeLinter.lint import NodeLinter, util
 
 
-class JadeLint(Linter):
+class JadeLint(NodeLinter):
 
     """Provides an interface to jade-lint."""
 
+    npm_name = 'jade-lint'
     syntax = 'jade'
-    cmd = 'jade-lint'
+    cmd = 'jade-lint @ *'
     executable = None
     version_args = '--version'
     version_re = r'(?P<version>\d+\.\d+\.\d+)'
-    version_requirement = '>= 0.0.2'
+    version_requirement = '>= 1.0.0'
     regex = r''
     multiline = False
-    line_col_base = (1, 1)
-    tempfile_suffix = None
+    # line_col_base = (1, 1)
+    tempfile_suffix = 'jade'
     error_stream = util.STREAM_BOTH
-    selectors = {}
-    word_re = None
-    defaults = {}
-    inline_settings = None
-    inline_overrides = None
-    comment_re = None
+    # selectors = {}
+    # word_re = None
+    # defaults = {}
+    # inline_settings = None
+    # inline_overrides = None
+    # comment_re = None
+    config_file = ('--config', '.jade-lintrc', '-')
 
