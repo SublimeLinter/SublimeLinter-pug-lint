5cd6ed09511fdd40714ebe647577cb77fd366f7f
Edditoria <edditoria@gmail.com>
2020-03-30 02:42:49 +0800

    Remove `error_stream`; Add `on_stderr` to make it works

--- linter.py
+++ linter.py
@@ -19,8 +19,8 @@ class PugLint(NodeLinter):
     cmd = 'pug-lint ${temp_file} ${args}'
     regex = r'^.+?:(?P<line>\d+)(:(?P<col>\d+) | )(?P<message>.+)'
     multiline = False
+    on_stderr = None
     tempfile_suffix = 'pug'
-    error_stream = util.STREAM_BOTH
     defaults = {
         'selector': 'text.pug, source.pypug, text.jade',
         '--reporter=': 'inline'
